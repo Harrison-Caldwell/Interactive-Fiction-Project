35bd1ce4736f9bc9bf6154d746745309fe5175f8
Harrison Caldwell <h.cald16@gmail.com>
2021-11-17 14:58:15 -0400

    Initial commit

---
+++ Interactive Fiction Project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Fiction_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string [] story = new string[]
+        {
+
+        }
+    }
+}
